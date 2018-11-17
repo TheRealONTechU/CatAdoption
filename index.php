13c084911aa7c9556753d215bd805957537a1181
Avdon Racki <avdon@racki.ca>
2018-11-17 17:47:03 -0500

    update file paths

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.html");
+    header("location: /html/welcome.html");
     exit;
 }
  
 // Include config file
-require_once "config.php";
+require_once "/php/config.php";
 
 function var_error_log( $object=null ){
     ob_start();                    // start buffer capture
@@ -72,7 +72,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.html");
+                            header("location: /html/welcome.html");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
