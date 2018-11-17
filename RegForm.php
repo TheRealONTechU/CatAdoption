646023147fb5b01f44479cc8dc7cd064e8b9ec53
Pujan <pujan.parikh@uoit.net>
2018-11-17 13:13:29 -0500

    update regform

--- RegForm.php
+++ RegForm.php
@@ -201,9 +201,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash;
             $param_email = $email;
-            $param_role = 'V';
+            $param_role = $code;
 
             if ($stmt -> execute()){
+                
                 $stmt->store_result();
             
             } else{
