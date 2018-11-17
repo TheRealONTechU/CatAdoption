8c2c18b3732e6882e698973afb7c36bd149d74c5
Pujan <pujan.parikh@uoit.net>
2018-11-17 03:08:49 -0500

    update mysql user and pass

--- Config.php
+++ Config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'rackite_tcradmin');
+define('DB_PASSWORD', 'tcr!management$');
 define('DB_NAME', 'usersDB');
  
 /* Attempt to connect to MySQL database */
