c56b2a68701b2231ded1a56aa3e806519520f535
Pujan <pujan.parikh@uoit.net>
2018-11-18 04:46:16 -0500

    fix

--- DeepValidation.php
+++ DeepValidation.php
@@ -1,6 +1,5 @@
 <?php
 
-        if ($_SESSION['username']== admin || $_SESSION['username'] != pujan824){
         if ($_SESSION['username']== admin || $_SESSION['username'] == pujan824){
 
             header("location: ../html/welcome.html");
