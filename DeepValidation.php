864081cf4f880407467e17cfc9b20e0892bef3db
HaraseesG <45112294+HaraseesG@users.noreply.github.com>
2018-11-18 08:25:18 -0500

    Update DeepValidation.php

--- DeepValidation.php
+++ DeepValidation.php
@@ -1,6 +1,6 @@
 <?php
 
-        if ($_SESSION['username']== admin || $_SESSION['username'] == pujan824){
+        if ($_SESSION['username']== 'admin' || $_SESSION['username'] == 'pujan824'){
             header("location: ../html/welcome.html");
         }else{
             header("location: ../html/WelcomeVolunteer.html");
