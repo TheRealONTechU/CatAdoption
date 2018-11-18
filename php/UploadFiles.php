616360342c844a6c44da371b39f6378ec78a8ced
Eric <ericwej@gmail.com>
2018-11-17 19:23:56 -0500

    Update UploadFiles.php
    uploadfile debug

--- php/UploadFiles.php
+++ php/UploadFiles.php
@@ -4,16 +4,18 @@ require_once "../config.php";
 
 if (isset($_POST['btn_upload'])){
 
+    $petpointID = $_POST['petpoint_id'];
     $filetmp = $_FILES["file_img"]["tmp_name"];
     $filename = $_FILES["file_img"]["name"];
     $filetype = $_FILES["file_img"]["type"];
     $filepath ="../uploads/".$filename;
-    $petpointID = 'petpoint_id';
 
     move_uploaded_file($filetmp, $filepath);
 
 $mysqli = "INSERT INTO FILE (file_name, file_path, file_type, petpoint_id)
             VALUES ('$filename', '$filepath' , '$filetype', $petpointID) ";
 $result = mysqli_query($mysqli,MYSQLI_STORE_RESULT($mysqli));
+    
+echo($petpointID);
 }
 ?>
