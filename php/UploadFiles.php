c34abca76034703cb8de46efcfdf47b379989b09
Eric <ericwej@gmail.com>
2018-11-18 00:03:21 -0500

    working file upload needs sql work
    needs to finish sql table mapping

--- php/UploadFiles.php
+++ php/UploadFiles.php
@@ -1,53 +1,42 @@
 <?php
+$doc = '';
 
+$petpointID = 'test';
 
+if(!empty($_FILES['cat_doc_upload'])) {
+    $doc = 'cat_doc_upload';
+} else if(!empty($_FILES['cat_pdf_upload'])) {
+    $doc = 'cat_pdf_upload';
+} else {
+    echo "The server encountered an issue with your file upload request, please try again later!";
+    return;
+}
 
-/*
-require_once "../config.php";
-
-//if (isset($_POST['btn_upload'])){
-    
-    $petpointID = 'hello';//$_POST['petpoint_id'];
-
-    $filetmp = $_FILES["file_img"]["tmp_name"];
-    $filename = $_FILES["file_img"]["name"];
-    $filetype = $_FILES["file_img"]["type"];
-   
-    $filepath ="../uploads/".$filename;
-
-    move_uploaded_file($filetmp, $filepath);
-
-    $mysqli = "INSERT INTO FILE (file_name, file_path, file_type, petpoint_id)
-            VALUES ('$filename', '$filepath' , '$filetype', $petpointID) ";
-    
-    $result = mysqli_query($mysqli,MYSQLI_STORE_RESULT($mysqli));
-
-    header("Location: ../html/CatDocumentUpload.html");
-
-/*} else {
-    echo("There was an erroe with your request :(");
-}*/ 
-
-if(!empty($_FILES['cat_picture_upload'])) {
+if($doc != '') {
     $path = '../uploads/';
-    $path = $path.basename($_FILES['cat_picture_upload']['name']);
-    $filename = $_FILES['cat_picture_upload']['name'];
+    $path = $path.basename($_FILES[$doc]['name']);
+    $filename = $_FILES[$doc]['name'];
+
     echo("found file, uploading...");
 
-    if(move_uploaded_file($_FILES['cat_picture_upload']['tmp_name'],$path)) {
+    if(move_uploaded_file($_FILES[$doc]['tmp_name'],$path)) {
         
-        echo(`Uploaded ${$filename} to temp dir`);
+        echo(`Uploaded to temp dir`);
 
-        $mysqli = "INSERT INTO FILE (file_name, file_path, file_type, petpoint_id) VALUES ('$filename', '$filepath' , '$filetype', $petpointID)";
+        $filename = $_FILES[$doc]['tmp_name'];
+        $filetype = $_FILES[$doc]['type'];
+
+        echo($filename);
+        echo($filetype);
+
+        $mysqli = "INSERT INTO FILE (file_name, file_path, file_type, petpoint_id) VALUES ('$filename', '$path' , '$filetype', $petpointID)";
         $result = mysqli_query($mysqli,MYSQLI_STORE_RESULT($mysqli));
         
-        echo(`and the final uploaded: ${$result}`);
-        header("Location: ../html/CatDocumentUpload.html");
+        echo(`and the final uploaded`);
     } else {
-        echo(`couldnt uplopad {$filename}`);
+        echo(`couldnt uplopad :(`);
     }
-} else {
-    echo "error :(";
 }
+echo "done";
 
 ?>
