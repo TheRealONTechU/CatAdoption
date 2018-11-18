b924687c34b2dde92cf287e1949e20557c40c0ba
Pujan <pujan.parikh@uoit.net>
2018-11-18 03:54:30 -0500

    change format

--- DeepValidation.php
+++ DeepValidation.php
@@ -1,12 +1,9 @@
 <?php
 
-
-    if ($_SESSION["username"]){
-
-        if ($username != admin || $username != pujan824){
-            header("location: ../WelcomeVolunteer.html");
-        }else{
+        if ($_SESSION['username']= admin || $_SESSION['username'] != pujan824){
             header("location: ../welcome.html");
+        }else{
+            header("location: ../WelcomeVolunteer.html");
         }
-    }
+
 ?>
